a49f0b243dc5efe7f4cd7e381ef61a3b21339169
Sweetvitta <vittoriadenovellis@gmail.com>
2025-02-19 09:47:30 +0100

    front accesible V.0.1

--- front/repo_analyzer/urls.py
+++ front/repo_analyzer/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('analyzer.urls')),
+    path('quick-analysis/', views.quick_analysis, name='quick_analysis'),
 ] 
